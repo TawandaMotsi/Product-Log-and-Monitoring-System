1984f16835126e22c57309239d187e74d5789320
Tawanda Motsi <tawaz08@gmail.com>
2025-08-08 10:19:27 +0000

    Initial commit for a java project

---
+++ src/test/java/com/example/Product_Log_and_Monitoring_System/ProductLogAndMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Product_Log_and_Monitoring_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductLogAndMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
